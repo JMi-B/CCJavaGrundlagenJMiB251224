978b579b28e206452d92e92e371572ca51e55709
JMi-B <j.zankl@gmx.de>
2024-12-26 16:02:12 +0100

    Erste Programmversuche

---
+++ src/thema1grundlagen/RechnerProgramm.java
@@ -0,0 +1,31 @@
+package thema1grundlagen;
+
+public class RechnerProgramm {
+	public static void main(String[] args) {
+	
+addierenUndAusgeben(10, 20);
+		
+		int zahl1 = 10;
+		int zahl2 = 20;
+		
+		int ergebnis = zahl1 + zahl2;
+		
+		System.out.println(" Addition von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+		
+		int zahl3 = 3;
+		int zahl4 = 13;
+		
+		int ergebnis2 = zahl3 + zahl4;
+		System.out.println(" Addition von " + zahl3 + " und " + zahl4 + " ist " + ergebnis2);
+		
+		// System.out.print(ergebnis);
+
+	}
+	
+	
+
+	public static void addierenUndAusgeben(int zahl1, int zahl2) {
+//		int ergebnis = zahl1 +zahl2;
+//		System.out.println(" Addition von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+	}
+	}
