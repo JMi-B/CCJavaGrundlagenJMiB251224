707821f395505c39e749f50c5ccff91be73acfb5
Juliane Bonenkamp <JBON182@more2learn.de>
2025-01-03 12:36:05 +0100

    Datentypen

---
+++ src/thema1grundlagen/klammer.java
@@ -0,0 +1,56 @@
+package thema1grundlagen;
+
+public class klammer {
+	public static void main (String [] arg) {
+		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		int x = 4;
+		int y = 5;
+		int z = 6;
+		
+		int n = 7;
+		int p = 8;
+		
+// Aufgabe 2.2 a 		
+		System.out.println((a+b)-(3*c));
+		System.out.println(a+b-(3*c));
+		System.out.println((a+b)-3*c);
+		System.out.println(a+b-3*c);
+
+// Aufgabe 2.2 b
+		System.out.println((4+x)/(y*z));
+		System.out.println(4+x/(y*z));
+		System.out.println((4+x)/y*z);
+		System.out.println(4+x/y*z);	
+		
+// Aufgabe 2.2 c
+		System.out.println((x+3)*(n%p));
+		System.out.println(x+3*(n%p));
+		System.out.println((x+3)*n%p);
+		System.out.println(x+3*n%p);
+
+// Aufgabe 2.2 d		
+		System.out.println(-a/(-(b+c)));
+		System.out.println(-a/-(b+c));
+		System.out.println(-a/-b+c);
+		System.out.println((-a)/-(b+c));
+		System.out.println((-a)/-b+c);
+
+// Aufgabe 2.2 e
+		System.out.println((x/y)%(-z));
+		System.out.println(x/y%(-z));
+		System.out.println((x/y)%-z);
+		
+		
+// aufgabe 2.2 f
+		System.out.println(x/(y%(-z)));
+		System.out.println(x/y%(-z));
+		System.out.println(x/(y%-z));
+		System.out.println(x/y%-z);
+		
+	}
+
+}
---
+++ src/thema1grundlagen/typen.java
@@ -0,0 +1,29 @@
+package thema1grundlagen;
+
+public class typen {
+	public static void main (String [] arg) {
+		
+				
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		
+		//2.3 a hat nichts mit den Variablen zu tun
+		//nicht  mit boelaen programmiet 
+		
+		// 2.3 b Ausgabe Int
+		System.out.println(10/3);
+		
+		// 2.3 c Ausgabe ist double
+		System.out.println(10/3.);
+		
+		// 2.3 d Tertäre Operatoren vergleichen Ausgabe boolan
+		System.out.println(x == y ? x > y : y < z);
+		
+		//2.3 f Ausgabe String
+		System.out.println(x + y + "z");
+		
+	}
+
+}
